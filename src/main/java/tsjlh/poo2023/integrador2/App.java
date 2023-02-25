2078b453161776afe752b8c0b235a7dc1c554bd9
Martín Aguilar <quienesmartin@outlook.com>
2023-02-24 22:16:53 -0600

    Primera iteración de menús

--- src/main/java/tsjlh/poo2023/integrador2/App.java
+++ src/main/java/tsjlh/poo2023/integrador2/App.java
@@ -1,7 +1,48 @@
 package tsjlh.poo2023.integrador2;
 
+// import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hola Mundo!");
+    // final ArrayList<User> users = new ArrayList<>();
+    final Scanner scanner = new Scanner(System.in);
+
+    int opcion = 0;
+
+    // Limpia la pantalla
+    System.out.print("\033[H\033[2J");
+
+    System.out.println("Bienvenido al sistema de almacenamiento del TSJ, UA: La Huerta.");
+
+    do {
+      /*
+       * Muestra un menú de opciones. Las opciones son:
+       * 1. Registro
+       * 2. Consulta
+       * 3. Salir
+       */
+      System.out.println("""
+          +-------Menú-------+
+          | 1. Registro      |
+          | 2. Consulta      |
+          | 3. Salir         |
+          +------------------+
+          """);
+
+      System.out.print("Ingrese una opción: ");
+      opcion = scanner.nextInt();
+
+      // Funcionalidad de menú
+      switch (opcion) {
+        case 1 -> Menu.add(scanner);
+        case 2 -> Menu.query(scanner);
+        case 3 -> System.out.println("Saliendo...");
+
+        default -> System.out.println("\033[31merror:\033[0m Opción inválida.");
+      }
+    } while (opcion != 3);
+
+    scanner.close();
   }
 }
---
+++ src/main/java/tsjlh/poo2023/integrador2/Menu.java
@@ -0,0 +1,11 @@
+package tsjlh.poo2023.integrador2;
+
+import java.util.Scanner;
+
+public class Menu {
+  public static void add(Scanner scanner) {
+  }
+
+  public static void query(Scanner scanner) {
+  }
+}
